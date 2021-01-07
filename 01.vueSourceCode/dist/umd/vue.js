4375c98ddafc8607cf7adba8ec73fc78b0556173
amazing T <>
2021-01-07 23:15:27 +0800

    html解析成ast语法树

--- 01.vueSourceCode/dist/umd/vue.js
+++ 01.vueSourceCode/dist/umd/vue.js
@@ -220,14 +220,243 @@
     observe(data);
   }
 
+  // <div>hello {{name}} <span>world</span></div>
+  // let html = {
+  //   tag: 'div',
+  //   parent: null,
+  //   type: 1,
+  //   attrs: [],
+  //   children: [
+  //     { tag: null, parent: '父div', attrs: [], text: 'hello {{name}}' },
+  //     { tag: 'span', parent: '父div', attrs: [], text: 'world' },
+  //   ]
+  // }
+  // src/core/util/lang.js
+  var unicodeRegExp = /a-zA-Z\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C-\u200D\u203F-\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD/; // src/compiler/parser/html-parser.js
+  // Regular Expressions for parsing tags and attributes
+
+  var attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/; // 匹配属性，aaa="aaa" | aaa='aaa' | aaa=aaa
+  var ncname = "[a-zA-Z_][\\-\\.0-9_a-zA-Z".concat(unicodeRegExp.source, "]*"); // 标签名: aaa-123aa
+  // ?:匹配不捕获
+
+  var qnameCapture = "((?:".concat(ncname, "\\:)?").concat(ncname, ")"); // 捕获标签 <my:xxx></my:xxx>
+
+  var startTagOpen = new RegExp("^<".concat(qnameCapture)); // 标签开头的正则 捕获内容是标签名，开始标签部分，不包含开始标签的结尾。如 <div class="className" ></div>，匹配的是 '<div class="className"'
+
+  var startTagClose = /^\s*(\/?)>/; // 匹配 > 标签 或者/> <div> <br/> 开始标签的结尾部分。如 <div class="className" ></div>，匹配的是 ' >'
+
+  var endTag = new RegExp("^<\\/".concat(qnameCapture, "[^>]*>")); // '</div><p></p>' 匹配结果为 </div>
+
+  function parseHTML(html) {
+    // 数据结构树 树、栈
+    function createASTElement(tagName, attrs) {
+      return {
+        tag: tagName,
+        // 标签名
+        type: 1,
+        // 元素类型
+        children: [],
+        // 孩子列表
+        attrs: attrs,
+        // 属性列表
+        parent: null // 父元素
+
+      };
+    }
+
+    var root; // 根节点
+
+    var currentParent; // 当前节点
+
+    var stack = []; // 存储标签的栈，用来判断栈是否符合预期
+    // 解析开始标签,并校验标签是否符合预期 <div><span></span></div> [div, span]
+
+    function start(tagName, attrs) {
+      console.log('开始标签', 'tagName', tagName, 'attrs', attrs);
+      var element = createASTElement(tagName, attrs);
+
+      if (!root) {
+        root = element;
+      }
+
+      currentParent = element; // 当前解析的标签 保存起来
+
+      stack.push(element); // 将生成的ast元素放到栈中
+    } // 解析结束标签(标签闭合的时候，才确定父子关系)
+
+
+    function end(tagName) {
+      console.log('结束标签-tagName', tagName); // 在结尾标签处创建父子关系
+
+      var element = stack.pop(); // 取出栈中的最后一个
+      // <div><p></p> hello </div> [div, p] currentParent=p, 取出p后，currentParent就是div
+
+      currentParent = stack[stack.length - 1];
+
+      if (currentParent) {
+        // 在闭合时，取出标签，可以知道取出这个标签的父亲是谁
+        element.parent = currentParent;
+        currentParent.children.push(element);
+      }
+    } // 解析文本
+
+
+    function chars(text) {
+      console.log('解析文本-text', text, "currentParent", currentParent);
+      text = text.replace(/\s/g, ''); // 去空格
+
+      if (text) {
+        currentParent.children.push({
+          type: 3,
+          // 文本类型用3
+          text: text
+        });
+      }
+    }
+
+    while (html) {
+      // 只要html不为空字符串，就一直解析
+      var textEnd = html.indexOf('<'); // 匹配到了标签的 "<"
+
+      if (textEnd == 0) {
+        // v-bind v-on <!DOCTYPE <!---->  <br/> 
+        //  肯定是标签
+        var startTagMatch = parseStartTag(); // 开始标签匹配的结果
+
+        if (startTagMatch) {
+          // 处理开始标签
+          start(startTagMatch.tagName, startTagMatch.attrs);
+        } // console.log("匹配掉一个开始标签之后---html", html)
+
+
+        var endTagMatch = html.match(endTag);
+
+        if (endTagMatch) {
+          // 处理结束标签
+          advance(endTagMatch[0].length);
+          end(endTagMatch[1]); // 将结束标签传入
+
+          continue;
+        } // break;
+
+      }
+
+      var text = 0;
+
+      if (textEnd >= 0) {
+        // 处理文本
+        text = html.substring(0, textEnd);
+      }
+
+      if (text) {
+        // 处理文本
+        advance(text.length);
+        chars(text); // console.log('html--->text', html)
+      } // break;
+
+    } // 将匹配过的字符串进行截取操作，再进行更新html
+
+
+    function advance(n) {
+      html = html.substring(n);
+    } // 解析标签
+
+
+    function parseStartTag() {
+      var start = html.match(startTagOpen);
+
+      if (start) {
+        // console.log(html, '匹配到的开始标签', start)
+        var match = {
+          tagName: start[1],
+          attrs: []
+        }; // console.log("match", match)
+
+        advance(start[0].length); // 已经进行过匹配到的开始标签，删除掉
+        // console.log('删除后的html', html)
+        // 如果是闭合标签，说明没有属性
+
+        var _end;
+
+        var attr; // 不是单标签，且能匹配到属性
+
+        while (!(_end = html.match(startTagClose)) && (attr = html.match(attribute))) {
+          // console.log('attr--->', attr)
+          match.attrs.push({
+            name: attr[1],
+            value: attr[3] || attr[4] || attr[5]
+          });
+          advance(attr[0].length); // 去掉当前已经匹配掉的属性
+          // console.log('match.attrs', match.attrs)
+          // break;
+        } // console.log('匹配的结束标签结尾---end', end)
+
+
+        if (_end) {
+          // >
+          // console.log('匹配的结束标签结尾---end', end)
+          advance(_end[0].length);
+          return match;
+        }
+      }
+    }
+
+    return root;
+  }
+
+  function compileToFunctions(template) {
+    // 将html模板 ==》 render函数
+    // ast语法树：用一颗树描述html结构，再把这样一个结构转换成js语法，最后生成对应的代码字符串 
+    // 虚拟dom和ast：一个对语法转义，一个对结构转义，ast可以描述css、js、dom，虚拟dom只能描述dom结构
+    // 虚拟dom 是用对象来描述节点，避免操作真实dom，来比对的； ast是描述代码的，描述语言本身,只描述语法：const a = 1; ==> {indentifier: const, name: a, value: 1}
+    // <div id="a"></div> ===> {attrs:[{id:'a'}], tag:'div', children:[]}
+    // 1.需要将html代码转化成"ast"语法树 可以用ast树来描述语言本身，
+    // 前端必须要掌握的数据结构（树）
+    var ast = parseHTML(template); // 2.通过这颗树 重新生成代码
+
+    console.log('compileToFunctions', 'template', template, 'ast', ast);
+  }
+
   function initMixin(Vue) {
     Vue.prototype._init = function (options) {
       var vm = this;
-      vm.$options = options; // 初始化状态 (将数据做一个初始化的劫持，当改变数据时，应该更新视图)
+      vm.$options = options; // 1.数据观测初始化(vue源码会先初始化事件和生命周期)
+      // 初始化状态 (将数据做一个初始化的劫持，当改变数据时，应该更新视图)
       // vue组件中有很多状态 data props watch computed
 
       initState(vm); // vue核心：响应式数据原理
       // MVVM：数据变化视图更新、视图变化影响数据(不能跳过数据更新视图)，$ref能实际直接操作dom
+      // 2. ast树解析
+      // 如果当前有el属性说明要渲染模板(el属性，等价于vm.$mount方法)
+
+      if (vm.$options.el) {
+        vm.$mount(vm.$options.el);
+      }
+    };
+
+    Vue.prototype.$mount = function (el) {
+      // 挂载操作
+      var vm = this;
+      var options = vm.$options;
+      el = document.querySelector(el); // debugger
+
+      if (!options.render) {
+        // 没render 将template转化成render方法
+        var template = options.template;
+
+        if (!template && el) {
+          template = el.outerHTML;
+        } // 有el、有template才做编译的操作，将模板转换成为render函数
+        // 编译原理 将模板编译成render函数
+
+
+        var render = compileToFunctions(template);
+        options.render = render;
+      }
+
+      console.log('最终渲染时都是options.render方法', options.render); // render方法优先级比template更高
+
+      console.log('el', el);
     };
   }
 
--- 01.vueSourceCode/src/init.js
+++ 01.vueSourceCode/src/init.js
@@ -1,18 +1,44 @@
 import Vue from ".";
 import { initState } from "./state";
+import { compileToFunctions } from "./compiler/index";
 
 // 扩展初始化功能
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
     const vm = this;
     vm.$options = options;
-
+    // 1.数据观测初始化(vue源码会先初始化事件和生命周期)
     // 初始化状态 (将数据做一个初始化的劫持，当改变数据时，应该更新视图)
     // vue组件中有很多状态 data props watch computed
     initState(vm)
-
-
     // vue核心：响应式数据原理
     // MVVM：数据变化视图更新、视图变化影响数据(不能跳过数据更新视图)，$ref能实际直接操作dom
+
+    // 2. ast树解析
+    // 如果当前有el属性说明要渲染模板(el属性，等价于vm.$mount方法)
+    if (vm.$options.el) {
+      vm.$mount(vm.$options.el)
+    }
+  }
+  Vue.prototype.$mount = function (el) {
+    // 挂载操作
+    const vm = this;
+    const options = vm.$options
+    el = document.querySelector(el)
+    // debugger
+    if (!options.render) {
+      // 没render 将template转化成render方法
+      let template = options.template
+      if (!template && el) {
+        template = el.outerHTML
+      }
+      // 有el、有template才做编译的操作，将模板转换成为render函数
+      // 编译原理 将模板编译成render函数
+      const render = compileToFunctions(template)
+      options.render = render
+    }
+    console.log('最终渲染时都是options.render方法', options.render)
+    // render方法优先级比template更高
+    console.log('el', el)
   }
 }
